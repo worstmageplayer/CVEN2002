65c2b6e1dd744075488b723c2adb70c7ad7b1667
worstmageplayer <shawnjun05@gmail.com>
2025-08-12 20:25:45 +1000

    wk2q3

--- NumericsWeek2.m
+++ NumericsWeek2.m
@@ -1,7 +1,7 @@
 %% Week 2
 clear all;
 % Question 1
-% Bisection
+% Bisection Method
 fprintf("Question 1\n");
 xl = 0.1500;
 xu = 0.9000;
@@ -38,6 +38,8 @@ while i <= 3
 end
 
 % Question 2
+% Newton Raphson Method
+fprintf("Question 2\n");
 Mm = 0.5;
 guess = 0.5;
 tolerance = 10 ^ -4;
@@ -60,3 +62,31 @@ for i = 1:100
 end
 
 fprintf("%.4f\n", guess);
+
+% Question 3
+% Secant Method
+fprintf("Question 3\n");
+g1 = 0.8625;
+g2 = 1.265;
+tolerance = 10 ^ -4;
+R = 1.15;
+
+syms f(x)
+f(x) = 4 * x * (2*R - sqrt( 4*R^2 - (x/4)^2)) - (x/4)^2;
+
+for i = 1:100
+    f1 = f(g1);
+    f2 = f(g2);
+
+    approx = g2 - f2 * (g2 - g1) / (f2 - f1);
+    err = abs((approx - g2) / approx);
+
+    g1 = g2;
+    g2 = approx;
+
+    if err < tolerance
+        break
+    end
+end
+
+fprintf("%.4f", approx);
